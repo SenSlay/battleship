d038700d555701fd708778a72e4db9db01116e45
Irvan Castro <121082999+SenSlay@users.noreply.github.com>
2024-09-29 20:53:25 +0800

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+  {
+    ignores: ['webpack.common.js', 'webpack.dev.js', 'webpack.prod.js'],
+  },
+];
