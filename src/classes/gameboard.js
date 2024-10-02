539f8ec6d80546dc3cc538f98a09b0a851c2b1b1
Irvan Castro <castroirvan@gmail.com>
2024-10-02 23:07:00 +0800

    add placeShip method
    initialize the board in the constructor
    make placeShip pass the current tests for x and y axis

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,7 +1,9 @@
 export default class Gameboard {
     #axisPlacement
+    #board
 
     constructor() {
+        this.#board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null));
         this.#axisPlacement = 'x'
     }
 
@@ -11,5 +13,21 @@ export default class Gameboard {
 
     switchAxis() {
         this.#axisPlacement = this.#axisPlacement === 'x' ? 'y' : 'x';
+    }
+
+    getBoard() {
+        return this.#board;
+    }
+
+    placeShip(ship, x, y) {
+        if (this.#axisPlacement === 'x') {
+            for (let i = x; i < x + ship.getLength(); i++) {
+                this.#board[y][i] = ship;
+            }
+        } else {
+            for (let i = y; i < y + ship.getLength(); i++) {
+                this.#board[i][x] = ship;
+            }
+        }
     }
 }
