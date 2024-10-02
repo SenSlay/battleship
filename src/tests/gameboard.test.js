c5f26db829f1a356d86aabf0f3857b4f0c6146e8
Irvan Castro <castroirvan@gmail.com>
2024-10-02 23:06:34 +0800

    create tests for placeShip method for x and y axis

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,4 +1,5 @@
 import Gameboard from "../classes/gameboard"
+import { ships } from '../classes/ship'
 
 describe('Gameboard class', () => {
     let gameboard;
@@ -19,4 +20,35 @@ describe('Gameboard class', () => {
     gameboard.switchAxis();
     expect(gameboard.getAxisPlacement()).toBe('x');
   });
+
+  describe('placeShip method', () => {
+    // test placeShip for x axis
+    test('placeShip places the ship horizontally on the board correctly', () => {
+      const x = 1;
+      const y = 0;
+      const destroyer = ships['destroyer'];
+      gameboard.placeShip(destroyer, x, y);
+      const destroyerLength = destroyer.getLength();
+  
+      for (let i = x; i < x + destroyerLength; i++) {
+        expect(gameboard.getBoard()[y][i]).toBe(destroyer); 
+      }
+    });
+  
+    // test placeShip for y axis
+    test('placeShip places the ship vertically on the board correctly', () => {
+      // switch to y axis
+      gameboard.switchAxis();
+  
+      const x = 1;
+      const y = 1;
+      const destroyer = ships['destroyer'];
+      gameboard.placeShip(destroyer, x, y);
+      const destroyerLength = destroyer.getLength();
+      
+      for (let i = y; i < y + destroyerLength; i++) {
+        expect(gameboard.getBoard()[i][x]).toBe(destroyer); 
+      }
+    });
+  })
 })
