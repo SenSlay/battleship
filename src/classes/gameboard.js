1cf17bfc744552812dce9ef1be13a9f1b886faa7
Irvan Castro <castroirvan@gmail.com>
2024-10-03 00:18:01 +0800

    create conditions for out of bounds in placeShip method
    run prettier

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,33 +1,60 @@
 export default class Gameboard {
-    #axisPlacement
-    #board
+  #axisPlacement;
+  #board;
 
-    constructor() {
-        this.#board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => null));
-        this.#axisPlacement = 'x'
+  constructor() {
+    this.#board = Array.from({ length: 10 }, () =>
+      Array.from({ length: 10 }, () => null),
+    );
+    this.#axisPlacement = 'x';
+  }
+
+  getAxisPlacement() {
+    return this.#axisPlacement;
+  }
+
+  switchAxis() {
+    this.#axisPlacement = this.#axisPlacement === 'x' ? 'y' : 'x';
+  }
+
+  getBoard() {
+    return this.#board;
+  }
+
+  placeShip(ship, x, y) {
+    const boardWidth = this.#board[0].length;
+    const boardHeight = this.#board.length;
+
+    if (this.#axisPlacement === 'x') {
+      // if out of bounds horizontally
+      if (
+        y < 0 ||
+        y >= boardHeight ||
+        x < 0 ||
+        x + ship.getLength() - 1 >= boardWidth
+      ) {
+        return false;
+      }
+
+      for (let i = x; i < x + ship.getLength(); i++) {
+        this.#board[y][i] = ship;
+      }
+    } else {
+      // if out of bounds vertically
+      if (
+        x < 0 ||
+        x >= boardWidth ||
+        y < 0 ||
+        y + ship.getLength() - 1 >= boardHeight
+      ) {
+        return false;
+      }
+
+      for (let i = y; i < y + ship.getLength(); i++) {
+        this.#board[i][x] = ship;
+      }
     }
 
-    getAxisPlacement() {
-        return this.#axisPlacement
-    }
-
-    switchAxis() {
-        this.#axisPlacement = this.#axisPlacement === 'x' ? 'y' : 'x';
-    }
-
-    getBoard() {
-        return this.#board;
-    }
-
-    placeShip(ship, x, y) {
-        if (this.#axisPlacement === 'x') {
-            for (let i = x; i < x + ship.getLength(); i++) {
-                this.#board[y][i] = ship;
-            }
-        } else {
-            for (let i = y; i < y + ship.getLength(); i++) {
-                this.#board[i][x] = ship;
-            }
-        }
-    }
+    return true;
+  }
 }
