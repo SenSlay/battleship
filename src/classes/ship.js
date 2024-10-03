6d01b7c5bf6086d967eb48eb14a66da1226b1e91
Irvan Castro <castroirvan@gmail.com>
2024-10-03 13:42:11 +0800

    remove the general ships object
    Decided to remove the ships object as it would produce bugs-
    the gameboards would be referencing the same ships object. Instead,
    I'm thinking of creating a ships object per each player.

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -23,12 +23,4 @@ class Ship {
   }
 }
 
-const ships = {
-    'carrier': new Ship(5),
-    'battleship': new Ship(4),
-    'destroyer': new Ship(3),
-    'submarine': new Ship(3),
-    'patrol boat': new Ship(2)
-}
-
-export { Ship as default, ships };
+export { Ship as default };
