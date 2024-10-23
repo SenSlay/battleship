d94073e035ea3d79cb2a419d0966868bce0ca293
Irvan Castro <castroirvan@gmail.com>
2024-10-23 13:39:13 +0800

    create the initial structure and design of ship placement page

---
+++ src/DOM/shipPlacement.js
@@ -0,0 +1,70 @@
+import { handleShipPlacement, toggleAxis, gameboardHoverEffect } from "../eventHandlers/shipPlacementHandlers";
+
+// Load ship placement grid
+const loadShipPlacement = () => {
+  const mainCtn = document.querySelector('.main');
+
+  // Clear main content
+  mainCtn.innerHTML = '';
+
+  // Create instructions heading el
+  const instructionsHeading = document.createElement('h1');
+  instructionsHeading.classList.add('instructions-heading');
+  instructionsHeading.textContent = 'Place your ship';
+
+  // Create switch axis button
+  const toggleAxisBtn = document.createElement('button');
+  toggleAxisBtn.classList.add('toggle-axis');
+  toggleAxisBtn.textContent = 'Toggle Axis: X';
+  toggleAxisBtn.addEventListener('click', toggleAxis);
+
+  // Create ship placement gameboard el
+  const gameboard = document.createElement('div');
+  gameboard.classList.add('gameboard');
+
+  // Attach the event listener to the gameboard container
+  gameboard.addEventListener('click', handleShipPlacement);
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const button = document.createElement('button');
+      button.dataset.x = j;
+      button.dataset.y = i;
+
+      gameboard.appendChild(button);
+    }
+  }
+
+  // Reset btn
+  const resetBtn = document.createElement('button');
+  resetBtn.textContent = 'Reset';
+
+  // Randomize btn
+  const randomizeBtn = document.createElement('button');
+  randomizeBtn.textContent = 'Randomize';
+
+  // Confirm btn
+  const confirmBtn = document.createElement('button');
+  confirmBtn.textContent = 'Confirm'
+
+  // Control btns container
+  const controlsCtn = document.createElement('div');
+  controlsCtn.classList.add('controls-ctn');
+  controlsCtn.appendChild(resetBtn);
+  controlsCtn.appendChild(randomizeBtn);
+  controlsCtn.appendChild(confirmBtn);
+
+  const shipPlacementCtn = document.createElement('div');
+  shipPlacementCtn.classList.add('ship-placement-ctn');
+
+  // Append ship placement content to main container
+  shipPlacementCtn.appendChild(instructionsHeading);
+  shipPlacementCtn.appendChild(toggleAxisBtn);
+  shipPlacementCtn.appendChild(gameboard);
+  shipPlacementCtn.appendChild(controlsCtn);
+ 
+  mainCtn.appendChild(shipPlacementCtn);
+  gameboardHoverEffect();
+};
+
+export default loadShipPlacement;
