c606920774e11477b2bc897bb52dab9c89998ba1
Irvan Castro <castroirvan@gmail.com>
2024-10-28 23:06:26 +0800

    implement checkWinner method create getter and resetter for game object and export those instead of the object

--- src/classes/game.js
+++ src/classes/game.js
@@ -8,6 +8,7 @@ class Game {
         this.#players.push(new HumanPlayer(playerOneName));
         this.#players.push(new ComputerPlayer());
         this.#activePlayer = this.#players[0];
+        this.#players[1].placeAllShips();
     }
 
     getPlayers() {
@@ -21,8 +22,33 @@ class Game {
     switchPlayerTurn() {
         this.#activePlayer = this.#activePlayer === this.#players[0] ? this.#players[1] : this.#players[0];
     }
+
+    checkWinner() {
+        const [playerOne, playerTwo] = this.#players;
+        
+        // Check if player one's ships are all sunk
+        if (playerOne.getGameboard().areAllShipsSunk()) {
+            return playerTwo; // Player Two wins
+        }
+        
+        // Check if player two's ships are all sunk
+        if (playerTwo.getGameboard().areAllShipsSunk()) {
+            return playerOne; // Player One wins
+        }
+        
+        // No winner yet
+        return null;
+    }
 }
 
-const game = new Game();
+let game = new Game();
 
-export { game };
+// Game getter
+const getGame = () => game;
+
+//  Game resetter
+const resetGame = () => {
+    game = new Game();
+};
+
+export { getGame, resetGame};
