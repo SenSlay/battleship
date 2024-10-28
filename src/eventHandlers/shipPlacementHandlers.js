a5cf688b70e3b97132595a2227bab160bd17f077
Irvan Castro <castroirvan@gmail.com>
2024-10-28 23:18:42 +0800

    implement functions for control buttons
    disable ship placement when all ships are placed
    create a function for initializing variables to allow smooth game reset

--- src/eventHandlers/shipPlacementHandlers.js
+++ src/eventHandlers/shipPlacementHandlers.js
@@ -1,42 +1,74 @@
-import { game } from '../classes/game';
+import { getGame } from '../classes/game';
+import loadGameStart from '../DOM/gameStart';
+import { DEFAULT_BTN_COLOR, SHIP_COLOR } from '../utils/constants';
 
-const DEFAULT_BTN_COLOR = 'rgb(218, 208, 208)';
-const SHIP_COLOR = 'lightblue';
+let game, playerOne, playerOneGameboard, ships;
 
-const playerOne = game.getPlayers()[0];
-const playerOneGameboard = playerOne.getGameboard();
-const ships = playerOne.getShips();
+export const initializeShipPlacementVariables = () => {
+  game = getGame();
+  playerOne = game.getPlayers()[0];
+  playerOneGameboard = playerOne.getGameboard();
+  ships = playerOne.getShips();
+}
+
 let shipIndex = 0;
-let currentShip = ships[shipIndex]
+let allShipsPlaced = false;
+
+const disableConfirmBtn = () => {
+  const confirmBtn = document.querySelector('.confirm-btn');
+  if (confirmBtn) {
+    confirmBtn.style.backgroundColor = 'lightgray';
+    confirmBtn.style.cursor = 'not-allowed';
+  }
+}
+
+const enableConfirmBtn = () => {
+  const confirmBtn = document.querySelector('.confirm-btn');
+  confirmBtn.style.backgroundColor = '#F0F0F0';
+  confirmBtn.style.cursor = 'pointer';
+}
+
+const updateInstructionsHeading = () => {
+  const heading = document.querySelector('.instructions-heading');
+  if (shipIndex >= 4 || allShipsPlaced) {
+    heading.textContent = 'Confirm Ship Placement';
+    return;
+  }
+  heading.textContent = `Place your ${ships[shipIndex].getName()}:`;
+}
 
 const handleShipPlacement = (event) => {
   const target = event.target;
 
   // Check if the clicked element is a button
-  if (target.tagName === 'BUTTON') {
+  if (target.tagName === 'BUTTON' && !allShipsPlaced) {
     const x = parseInt(target.dataset.x);
     const y = parseInt(target.dataset.y);
-    currentShip = ships[shipIndex];
+    let currentShip = ships[shipIndex];
     
-    // If all ships have been placed
-    if (shipIndex >= ships.length) {
-      console.log('All ships placed');
-      return;
-    }
-
     // Check if ship placement is valid
     if (!playerOneGameboard.isShipPlacementValid(currentShip, x, y)) {
       return;
     }
 
-    // Place ship in player gameboard
-    console.log(x, y)
-    playerOne.placeShip(currentShip, x, y);
+    // If more ships to be placed
+    if (shipIndex < ships.length) {
+      playerOne.placeShip(currentShip, x, y);
+      shipIndex++;  
+      updateInstructionsHeading();
+    } 
+
+    // If all ships have been placed
+    if (shipIndex >= ships.length) {
+      console.log('All ships placed');
+      allShipsPlaced = true;
+      enableConfirmBtn();
+      return;
+    }
 
     if (playerOneGameboard.getAxisPlacement() === 'x') {
       // If axis placement is x...
-      console.log(currentShip);
-
+      
       for (let i = 0; i < currentShip.getLength(); i++) {
         console.log(`Placing ship at: (${x + i}, ${y})`);
 
@@ -48,7 +80,6 @@ const handleShipPlacement = (event) => {
       }
     } else {
       // Else if axis placement is y...
-      console.log(currentShip);
 
       for (let i = 0; i < currentShip.getLength(); i++) {
         console.log(`Placing ship at: (${x}, ${y + i})`);
@@ -60,9 +91,6 @@ const handleShipPlacement = (event) => {
         nextButton.style.backgroundColor = SHIP_COLOR;
       }
     }
-
-    // If placement is successful, move to the next ship
-    shipIndex++;
   }
 };
 
@@ -81,15 +109,15 @@ const gameboardHoverEffect = () => {
   const gameboard = document.querySelector('.gameboard');
 
   gameboard.addEventListener('mouseover', (event) => {
+    if (shipIndex === ships.length) return;
     const target = event.target;
     const x = parseInt(target.dataset.x);
     const y = parseInt(target.dataset.y);
 
     // If hovered is a button
     if (target.tagName === 'BUTTON') {
-      console.log('hovering')
-
-      if (!playerOneGameboard.isShipPlacementValid(ships[shipIndex], x, y) || playerOneGameboard.getBoard()[y][x].ship !== null) {
+      // If cell is invalid or contains a ship
+      if (!playerOneGameboard.isShipPlacementValid(ships[shipIndex], x, y)) {
         target.style.cursor = 'not-allowed';
         target.style.backgroundColor = 'red'
         return;
@@ -103,7 +131,7 @@ const gameboardHoverEffect = () => {
           const nextButton = document.querySelector(
             `button[data-x="${x + i}"][data-y="${y}"]`,
           );
-          nextButton.style.backgroundColor = 'lightblue';
+          nextButton.style.backgroundColor = SHIP_COLOR;
         }
       } else {
         // Else if axis placement is y
@@ -113,24 +141,20 @@ const gameboardHoverEffect = () => {
           const nextButton = document.querySelector(
             `button[data-x="${x}"][data-y="${y + i}"]`,
           );
-          nextButton.style.backgroundColor = 'lightblue';
+          nextButton.style.backgroundColor = SHIP_COLOR;
         }
       }
     }
   })
 
   gameboard.addEventListener('mouseout', (event) => {
+    if (shipIndex === ships.length) return;
     const target = event.target;
     const x = parseInt(target.dataset.x);
     const y = parseInt(target.dataset.y);
-    console.log(playerOneGameboard.getAxisPlacement());
-
-    console.log(playerOneGameboard.getBoard()[x][y])
   
     // If leaving a button
     if (target.tagName === 'BUTTON') {
-      console.log('leaving');
-  
       if (!playerOneGameboard.isShipPlacementValid(ships[shipIndex], x, y)) {
         target.style.cursor = 'pointer';
 
@@ -169,4 +193,52 @@ const gameboardHoverEffect = () => {
   });
 };
 
-export { handleShipPlacement, toggleAxis, gameboardHoverEffect };
+const resetShipPlacement = () => {
+  playerOneGameboard.clearShips();
+  shipIndex = 0;
+  allShipsPlaced = false;
+  disableConfirmBtn();
+  updateInstructionsHeading();
+
+  // Set gameboard UI back to default state 
+  const gameboardBtns = document.querySelectorAll('.gameboard button');
+  gameboardBtns.forEach(btn => {
+    btn.style.backgroundColor = DEFAULT_BTN_COLOR;
+  })
+}
+
+const controlHandlers = () => {
+  const controlCtn = document.querySelector('.controls-ctn');
+
+  controlCtn.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.closest('.reset-btn')) {
+      resetShipPlacement();
+    } else if (target.closest('.randomize-btn')) {
+      resetShipPlacement();
+      playerOne.placeAllShips();
+      allShipsPlaced = true;
+      enableConfirmBtn();
+      updateInstructionsHeading();
+      
+      const gameboardBtns = document.querySelectorAll('.gameboard button');
+      gameboardBtns.forEach(btn => {
+        const x = parseInt(btn.dataset.x);
+        const y = parseInt(btn.dataset.y);
+        
+        if (playerOneGameboard.getBoard()[y][x].ship !== null) {
+          btn.style.backgroundColor = SHIP_COLOR;
+        }
+      })
+      shipIndex = 5;
+    }
+    else if (target.closest('.confirm-btn')) {
+      if (allShipsPlaced) {
+        loadGameStart();
+      }
+    }
+  })
+}
+
+export { handleShipPlacement, toggleAxis, gameboardHoverEffect, controlHandlers, disableConfirmBtn, updateInstructionsHeading, resetShipPlacement };
