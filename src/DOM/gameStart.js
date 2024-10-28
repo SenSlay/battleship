3412695d0020266657c676f1d5b6b55fb117c2a1
Irvan Castro <castroirvan@gmail.com>
2024-10-29 02:06:00 +0800

    implement loadGameStart for game start page

---
+++ src/DOM/gameStart.js
@@ -0,0 +1,110 @@
+import { getGame } from '../classes/game';
+import {
+  handleAttack,
+  initializeGameStartVariables,
+} from '../eventHandlers/gameStartHandlers';
+import { SHIP_COLOR } from '../utils/constants';
+
+const loadGameStart = () => {
+  const playerOneBoard = getGame().getPlayers()[0].getGameboard().getBoard();
+  const playerTwoBoard = getGame().getPlayers()[1].getGameboard().getBoard();
+
+  initializeGameStartVariables();
+  const mainCtn = document.querySelector('.main');
+
+  // Clear main content
+  mainCtn.innerHTML = '';
+
+  // Create friendly span
+  const friendlySpan = document.createElement('span');
+  friendlySpan.classList.add('friendly-attack-status');
+  friendlySpan.textContent = 'Attacking...';
+
+  // Create friendly instructions heading el
+  const friendlyHeading = document.createElement('h1');
+  friendlyHeading.classList.add('instructions-heading', 'friendly-heading');
+  friendlyHeading.textContent = 'Friendly: ';
+  friendlyHeading.appendChild(friendlySpan);
+
+  // Create friendly span
+  const enemySpan = document.createElement('span');
+  enemySpan.classList.add('enemy-attack-status');
+  enemySpan.textContent = 'Attacking...';
+
+  // Create enemy instructions heading el
+  const enemyHeading = document.createElement('h1');
+  enemyHeading.classList.add('instructions-heading', 'enemy-heading');
+  enemyHeading.textContent = 'Enemy: ';
+  enemyHeading.appendChild(enemySpan);
+
+  // Create friendly gameboard el
+  const friendlyGameboard = document.createElement('div');
+  friendlyGameboard.classList.add('gameboard', 'friendly-board');
+
+  // Create enemyGameboard el
+  const enemyGameboard = document.createElement('div');
+  enemyGameboard.classList.add('gameboard', 'enemy-board');
+  enemyGameboard.addEventListener('click', handleAttack);
+
+  // Append the buttons to gameboards
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      // Create and append a button for friendlyGameboard
+      const friendlyButton = document.createElement('button');
+      friendlyButton.dataset.x = j;
+      friendlyButton.dataset.y = i;
+
+      // Highlight cells with ships placed
+      if (playerOneBoard[i][j].ship !== null) {
+        friendlyButton.style.backgroundColor = SHIP_COLOR;
+      }
+      friendlyGameboard.appendChild(friendlyButton);
+
+      // Create and append a button for enemyGameboard
+      const enemyButton = document.createElement('button');
+      enemyButton.dataset.x = j;
+      enemyButton.dataset.y = i;
+
+      // Highlight cells with ships placed
+      if (playerTwoBoard[i][j].ship !== null) {
+        enemyButton.style.backgroundColor = SHIP_COLOR;
+      }
+
+      enemyGameboard.appendChild(enemyButton);
+    }
+  }
+
+  const friendlyBoardLabel = document.createElement('h2');
+  friendlyBoardLabel.classList.add('board-label');
+  friendlyBoardLabel.textContent = 'Friendly Waters';
+
+  const enemyBoardLabel = document.createElement('h2');
+  enemyBoardLabel.classList.add('board-label');
+  enemyBoardLabel.textContent = 'Enemy Waters';
+
+  const friendlyCtn = document.createElement('div');
+  friendlyCtn.classList.add('friendly-ctn');
+  friendlyCtn.appendChild(friendlyHeading);
+  friendlyCtn.appendChild(friendlyBoardLabel);
+  friendlyCtn.appendChild(friendlyGameboard);
+
+  const enemyCtn = document.createElement('div');
+  enemyCtn.classList.add('enemy-ctn');
+  enemyCtn.appendChild(enemyHeading);
+  enemyCtn.appendChild(enemyBoardLabel);
+  enemyCtn.appendChild(enemyGameboard);
+
+  const gameboardsCtn = document.createElement('div');
+  gameboardsCtn.classList.add('gameboards-ctn');
+  gameboardsCtn.appendChild(friendlyCtn);
+  gameboardsCtn.appendChild(enemyCtn);
+
+  const gameStartCtn = document.createElement('div');
+  gameStartCtn.classList.add('game-start-ctn');
+
+  gameStartCtn.appendChild(gameboardsCtn);
+
+  mainCtn.appendChild(gameStartCtn);
+};
+
+export default loadGameStart;
