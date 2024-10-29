cb87214beb3566319952f812410b29a188dc0657
Irvan Castro <castroirvan@gmail.com>
2024-10-29 20:07:17 +0800

    implement highlightShips function

--- src/utils/DOMUtils.js
+++ src/utils/DOMUtils.js
@@ -28,4 +28,29 @@ const createWinnerDisplayCtn = (playerName) => {
     return winnerCtn;
 }
 
-export { createWinnerDisplayCtn };
+// Create gameboard with ships highlighted
+const highlightShips = (gameboard, gameboardBtns) => {
+    gameboardBtns.forEach(btn => {
+        const x = parseInt(btn.dataset.x);
+        const y = parseInt(btn.dataset.y);
+        const cell = gameboard.getBoard()[y][x];
+
+        if (cell.ship !== null) {
+            const ship = cell.ship;
+            const shipCells = gameboard.getShipCoordinates(ship);
+            const shipIndex = shipCells.findIndex(([sx, sy]) => sx === x && sy === y);
+            const shipAxis = ship.getAxis();
+
+            // Determine class to apply based on position within the ship
+            if (shipIndex === 0) {
+            btn.classList.add(shipAxis === 'x' ? 'start-ship-x' : 'start-ship-y');
+            } else if (shipIndex > 0 && shipIndex < shipCells.length - 1) {
+            btn.classList.add(shipAxis === 'x' ? 'mid-ship-x' : 'mid-ship-y');
+            } else {
+            btn.classList.add(shipAxis === 'x' ? 'end-ship-x' : 'end-ship-y');
+            }
+        }
+    }); 
+}
+
+export { createWinnerDisplayCtn, highlightShips };
